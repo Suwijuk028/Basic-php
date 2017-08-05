977f53ee15483f400be00549784b9b3a5f3f28e9
Suwijuk028 <suwijuk029@hotmail.com>
2017-08-05 21:01:51 +0700

    Condition
    การตรวจเช็คเงื่อนไข

---
+++ condition.php
@@ -0,0 +1,49 @@
+<?php
+//ตรวจสอบเงื่อนไข
+
+/* if else
+$score=500;
+if($score==500){
+    echo "ตัวเลขถูกต้อง";
+  }
+  else{
+      echo "ตัวเลขไม่ถูกต้อง";
+  }
+ ?>
+ */
+
+/* if if ตรวจเช็คทุกเงื่อนไข ถ้าไม่มี else
+  $score=500;
+  if($score==500){
+    echo "ตัวเลขถูกต้อง";
+  }
+  if($score==400){
+    echo "ตัวเลขเท่ากับ 400";
+  }
+  if($score==300){
+    echo "ตัวเลขเท่ากับ 300";
+  }
+  */
+
+  /*  else if
+  $score=500;
+  if($score==500){
+    echo "ตัวเลขถูกต้อง";
+  }
+  else if($score==400){
+    echo "ตัวเลขเท่ากับ 400";
+  }
+  else if($score==300){
+    echo "ตัวเลขเท่ากับ 300";
+  }
+  */
+
+/* switch case
+  $score=500;
+  switch($score){
+    case 300:echo "300";break;
+    case 400:echo "400";break;
+    case 500:echo "500";break;
+    default : echo "ไม่ตรงกับค่าไหนเลย";
+  }
+  */
