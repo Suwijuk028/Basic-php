dfc8fa4b631e7044d2ae4fc3dde236263016e5d7
Suwijuk028 <suwijuk029@hotmail.com>
2017-08-06 17:29:54 +0700

    loop
    การวนลูป

---
+++ loop.php
@@ -0,0 +1,31 @@
+<?php
+  /* คำสั่ง for ทำซ้ำ
+  for($count=1;$count<=20;$count++){
+    echo "Bank<br>";
+
+  }
+*/
+
+//for($run=1;$run<=10;$run++){
+  //echo "Suwijuk<br>";
+}
+  /* คำสั่ง while
+  $score=20;
+  while ($score!=25){
+    echo "Suwijuk<br>";
+    $score++;
+    if ($score==25){
+    break;
+  }
+  }
+*/
+
+  //  คำสั่ง do while
+  $score=1;
+  do{
+    echo "Programmimg<br>";
+    $score++;
+  }
+  while($score<=10);
+
+ ?>
